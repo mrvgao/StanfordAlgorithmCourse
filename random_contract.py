f10bfc3a8710dfc5be437d3895d18f73f76bdfaa
fortymiles <mqgao@outlook.com>
2017-11-30 16:00:14 +0800

    fix random seed error

--- random_contract.py
+++ random_contract.py
@@ -7,17 +7,16 @@ from copy import deepcopy
 import numpy as np
 
 
-def random_select_vertex(graph, num=1, seed=None):
-    if seed: random.seed(0)
+def random_select_vertex(graph, num=1):
     vertices = graph.get_vertices()
     random.shuffle(vertices)
     if num == 1: return vertices[0]
     else: return vertices[:num]
 
 
-def random_contract_one_pass(graph: Graph, seed=None):
+def random_contract_one_pass(graph: Graph):
     while len(graph.get_vertices()) > 2:
-        random_vertice_1, random_vertice_2 = random_select_vertex(graph, num=2, seed=seed)
+        random_vertice_1, random_vertice_2 = random_select_vertex(graph, num=2)
         new_vertex = graph.merge_vertices([random_vertice_1, random_vertice_2])
         graph.remove_one_vertex_self_cycle(new_vertex)
     return graph
@@ -32,7 +31,8 @@ def random_contract(g: Graph, verboes=False):
     run_times = int(constant * run_times)
     min_cuts = float('inf')
     for i in range(run_times):
-        tmp_g = random_contract_one_pass(original_g, seed=i)
+        random.seed(i)
+        tmp_g = random_contract_one_pass(original_g)
         cuts = len(tmp_g.adjacency[tmp_g.get_vertices()[0]])
         if cuts < min_cuts: min_cuts = cuts
         original_g = deepcopy(g)
